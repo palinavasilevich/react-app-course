29250187060652ea070f22362eb0f9a72ba4bca2
pvslvch <polina.vasilevichh@gmail.com>
2025-04-25 14:15:29 +0200

    feat: implement simulation of server request delay

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,14 @@ import axios from "axios";
 
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList";
+import { Loader } from "../../components/Loader";
+import { delayFn } from "../../helpers/delayFn";
 
 // import cls from "./HomePage.module.css";
 
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getQuestions();
@@ -15,15 +18,23 @@ export const HomePage = () => {
 
   const getQuestions = async () => {
     try {
+      setIsLoading(true);
+
+      // Simulate server request delay
+      await delayFn();
+
       const { data: questions } = await axios.get(`${API_URL}/questions`);
       setQuestions(questions);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <>
+      {isLoading && <Loader />}
       <QuestionCardList questions={questions} />
     </>
   );

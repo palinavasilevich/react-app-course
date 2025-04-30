54c76bce1936c4a2a32d81b65ad78ba03ee9dcc1
pvslvch <polina.vasilevichh@gmail.com>
2025-04-30 12:58:09 +0200

    feat: implement Protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import { HomePage } from "./pages/HomePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
@@ -6,6 +13,19 @@ import { QuestionPage } from "./pages/QuestionPage/QuestionPage";
 import { AddQuestionPageLazy } from "./pages/AddQuestionPage";
 import { EditQuestionPage } from "./pages/EditQuestionPage/EditQuestionPage";
 import { AuthProvider } from "./auth/AuthProvider";
+import { useAuth } from "./hooks/useAuth";
+import { ForbiddenPage } from "./pages/ForbiddenPage/ForbiddenPage";
+
+const ProtectedRoutes = () => {
+  const { isAuth } = useAuth();
+  const location = useLocation();
+
+  return isAuth ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/forbidden" state={{ from: location.pathname }} replace />
+  );
+};
 
 function App() {
   return (
@@ -14,10 +34,13 @@ function App() {
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<HomePage />} />
-            <Route path="/forbidden" element={<div>FORBIDDEN PAGE</div>} />
-            <Route path="/addquestion" element={<AddQuestionPageLazy />} />
+            <Route path="/forbidden" element={<ForbiddenPage />} />
             <Route path="/question/:id" element={<QuestionPage />} />
-            <Route path="/editquestion/:id" element={<EditQuestionPage />} />
+
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/addquestion" element={<AddQuestionPageLazy />} />
+              <Route path="/editquestion/:id" element={<EditQuestionPage />} />
+            </Route>
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
